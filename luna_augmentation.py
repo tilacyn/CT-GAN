004b638ef44db044c40e9adeadc158f1bedd640a
Maksim Kryuchkov <mkryuchkov@devexperts.com>
2020-05-22 22:49:46 +0300

    added augmentation service

---
+++ luna_augmentation.py
@@ -0,0 +1,45 @@
+from procedures.attack_pipeline import *
+from os.path import join as opjoin
+
+
+class AugmentationService:
+    '''
+
+    '''
+
+    def __init__(self, scan_paths, generator_path, save_dir):
+        self.generator_path = generator_path
+        self.save_dir = save_dir
+        self.inject_coords_resolver = InjectCoordinatesResolver()
+        inject_coords = [self.inject_coords_resolver.resolve(path2scan) for path2scan in scan_paths]
+        self.instances = [Instance(scan_path, inject_coord) for scan_path, inject_coord in
+                          zip(scan_paths, inject_coords)]
+
+    def load_generator(self):
+        self.injector = scan_manipulator(self.generator_path)
+        self.injector.load_injector()
+
+    def augment(self):
+        for instance in self.instances:
+            self.injector.load_target_scan(instance.path2scan)
+            self.injector.tamper(instance.inject_coords, isVox=True)
+            self.injector.save_tampered_scan(self.save_dir, instance.get_save_filename())
+
+
+class Instance:
+    def __init__(self, path2scan, inject_coord):
+        self.path2scan = path2scan
+        self.inject_coords = inject_coord
+
+    def get_save_filename(self):
+        return 'generated_' + self.path2scan.split('/')[-1]
+
+
+class InjectCoordinatesResolver:
+    def __init__(self):
+        pass
+
+    def resolve(self, path2scan):
+        path2label = path2scan[:-9] + 'label.npy'
+        label = np.load(path2label)
+        return label.astype(np.int32)
--- procedures/attack_pipeline.py
+++ procedures/attack_pipeline.py
@@ -35,12 +35,12 @@ import scipy.ndimage
 from utils.dicom_utils import *
 from utils.utils import *
 
-
 # in this version: coords must be provided manually (for autnomaic candiate location selection, use[x])
 # in this version: we scale the entire scan. For faster tampering, one should only scale the cube that is being tampred.
 # in this version: dicom->dicom, dicom->numpy, mhd/raw->numpy supported
 
 MODEL_PATH_INJECT = config['modelpath_inject']
+
 
 class scan_manipulator:
     def __init__(self, model_inj_path):
@@ -57,9 +57,12 @@ class scan_manipulator:
         self.model_rem_path = config['modelpath_remove']
 
         # load models
+
+    def load_injector(self):
         print("Loading models")
         if os.path.exists(os.path.join(self.model_inj_path, "G_model.h5")):
-            self.generator_inj = load_model(os.path.join(self.model_inj_path, "G_model.h5"), custom_objects={'ktf' : ktf})
+            self.generator_inj = load_model(os.path.join(self.model_inj_path, "G_model.h5"),
+                                            custom_objects={'ktf': ktf})
             # load normalization params
             self.norm_inj = np.load(os.path.join(MODEL_PATH_INJECT, 'normalization.npy'))
             # load equalization params
@@ -68,16 +71,6 @@ class scan_manipulator:
         else:
             self.generator_inj = None
             print("Failed to Load Injector Model")
-        if os.path.exists(os.path.join(self.model_rem_path, "G_model.h5")):
-            self.generator_rem = load_model(os.path.join(self.model_rem_path, "G_model.h5"))
-            # load normalization params
-            self.norm_rem = np.load(os.path.join(self.model_rem_path, 'normalization.npy'))
-            # load equalization params
-            self.eq_rem = histEq([], path=os.path.join(self.model_rem_path, 'equalization.pkl'))
-            print("Loaded Remover Model")
-        else:
-            self.generator_rem = None
-            print("Failed to Load Remover Model")
 
     # loads dicom/mhd to be tampered
     # Provide path to a *.dcm file or the *mhd file. The contaitning folder should have the other slices)
@@ -89,7 +82,7 @@ class scan_manipulator:
         self.scan = self.scan.astype(float)
 
     # saves tampered scan as 'dicom' series or 'numpy' serialization
-    def save_tampered_scan(self, save_dir, output_type='dicom'):
+    def save_tampered_scan(self, save_dir, filename, output_type='dicom'):
         if self.scan is None:
             print('Cannot save: load a target scan first.')
             return
@@ -103,85 +96,73 @@ class scan_manipulator:
                 save_dicom(self.scan, origional_raw_slices=self.scan_raw_slices, dst_directory=save_dir)
         else:  # save as numpy
             os.makedirs(save_dir, exist_ok=True)
-            np.save(os.path.join(save_dir, 'tampered_scan.np'), self.scan)
+            np.save(os.path.join(save_dir, filename), self.scan)
         print('Done.')
 
     # tamper loaded scan at given voxel (index) coordinate
     # coord: E.g. vox: slice_indx, y_indx, x_indx    world: -324.3, 23, -234
     # action: 'inject' or 'remove'
-    def tamper(self, coord, action="inject", isVox=True):
-        if self.scan is None:
-            print('Cannot tamper: load a target scan first.')
-            return
-        if (action == 'inject') and (self.generator_inj is None):
-            print('Cannot inject: no injection model loaded.')
-            return
-        if (action == 'remove') and (self.generator_rem is None):
-            print('Cannot inject: no removal model loaded.')
-            return
+    def tamper(self, coord, isVox=True):
+        def cut_target(coord):
+            print("Cutting out target region")
+            cube_shape = get_scaled_shape(config["cube_shape"], 1 / self.scan_spacing)
+            clean_cube_unscaled = cutCube(self.scan, coord, cube_shape)
+            clean_cube, resize_factor = scale_scan(clean_cube_unscaled, self.scan_spacing)
+            # Store backup reference
+            sdim = int(np.max(cube_shape) * 1.3)
+            clean_cube_unscaled2 = cutCube(self.scan, coord, np.array([sdim, sdim, sdim]))  # for noise touch ups later
+            return clean_cube, resize_factor, clean_cube_unscaled, clean_cube_unscaled2
 
-        if action == 'inject':
-            print('===Injecting Evidence===')
-        else:
-            print('===Removing Evidence===')
+        def equalize(clean_cube):
+            print("Normalizing sample")
+            clean_cube_eq = self.eq_inj.equalize(clean_cube)
+            clean_cube_norm = (clean_cube_eq - self.norm_inj[0]) / ((self.norm_inj[2] - self.norm_inj[1]))
+            return clean_cube_norm
+
+        def inject(clean_cube_norm):
+            print("Injecting evidence")
+
+            x = np.copy(clean_cube_norm)
+            x[self.m_zlims[0]:self.m_zlims[1], self.m_xlims[0]:self.m_xlims[1], self.m_ylims[0]:self.m_ylims[1]] = 0
+            x = x.reshape((1, config['cube_shape'][0], config['cube_shape'][1], config['cube_shape'][2], 1))
+            x_mal = self.generator_inj.predict([x])
+            x_mal = x_mal.reshape(config['cube_shape'])
+            return x_mal
+
+        def deequalize(x_mal):
+            print("De-normalizing sample")
+            x_mal[x_mal > .5] = .5  # fix boundry overflow
+            x_mal[x_mal < -.5] = -.5
+            mal_cube_eq = x_mal * ((self.norm_inj[2] - self.norm_inj[1])) + self.norm_inj[0]
+            mal_cube = self.eq_inj.dequalize(mal_cube_eq)
+            # Correct for pixel norm error
+            # fix overflow
+            bad = np.where(mal_cube > 2000)
+            # mal_cube[bad] = np.median(clean_cube)
+            for i in range(len(bad[0])):
+                neiborhood = cutCube(mal_cube, np.array([bad[0][i], bad[1][i], bad[2][i]]),
+                                     (np.ones(3) * 5).astype(int),
+                                     -1000)
+                mal_cube[bad[0][i], bad[1][i], bad[2][i]] = np.mean(neiborhood)
+            # fix underflow
+            mal_cube[mal_cube < -1000] = -1000
+            return mal_cube
+
+        print('===Injecting Evidence===')
         if not isVox:
             coord = world2vox(coord, self.scan_spacing, self.scan_orientation, self.scan_origin)
 
         ### Cut Location
-        print("Cutting out target region")
-        cube_shape = get_scaled_shape(config["cube_shape"], 1 / self.scan_spacing)
-        clean_cube_unscaled = cutCube(self.scan, coord, cube_shape)
-        clean_cube, resize_factor = scale_scan(clean_cube_unscaled, self.scan_spacing)
-        # Store backup reference
-        sdim = int(np.max(cube_shape) * 1.3)
-        clean_cube_unscaled2 = cutCube(self.scan, coord, np.array([sdim, sdim, sdim]))  # for noise touch ups later
-
+        clean_cube, resize_factor, clean_cube_unscaled, clean_cube_unscaled2 = cut_target(coord)
         ### Normalize/Equalize Location
-        print("Normalizing sample")
-        if action == 'inject':
-            clean_cube_eq = self.eq_inj.equalize(clean_cube)
-            clean_cube_norm = (clean_cube_eq - self.norm_inj[0]) / ((self.norm_inj[2] - self.norm_inj[1]))
-        else:
-            clean_cube_eq = self.eq_rem.equalize(clean_cube)
-            clean_cube_norm = (clean_cube_eq - self.norm_rem[0]) / ((self.norm_rem[2] - self.norm_rem[1]))
-
+        clean_cube_norm = equalize(clean_cube)
         ########  Inject Cancer   ##########
 
         ### Inject/Remove evidence
-        if action == 'inject':
-            print("Injecting evidence")
-        else:
-            print("Removing evidence")
-
-        x = np.copy(clean_cube_norm)
-        x[self.m_zlims[0]:self.m_zlims[1], self.m_xlims[0]:self.m_xlims[1], self.m_ylims[0]:self.m_ylims[1]] = 0
-        x = x.reshape((1, config['cube_shape'][0], config['cube_shape'][1], config['cube_shape'][2], 1))
-        if action == 'inject':
-            x_mal = self.generator_inj.predict([x])
-        else:
-            x_mal = self.generator_rem.predict([x])
-        x_mal = x_mal.reshape(config['cube_shape'])
+        x_mal = inject(clean_cube_norm)
 
         ### De-Norm/De-equalize
-        print("De-normalizing sample")
-        x_mal[x_mal > .5] = .5  # fix boundry overflow
-        x_mal[x_mal < -.5] = -.5
-        if action == 'inject':
-            mal_cube_eq = x_mal * ((self.norm_inj[2] - self.norm_inj[1])) + self.norm_inj[0]
-            mal_cube = self.eq_inj.dequalize(mal_cube_eq)
-        else:
-            mal_cube_eq = x_mal * ((self.norm_rem[2] - self.norm_rem[1])) + self.norm_rem[0]
-            mal_cube = self.eq_rem.dequalize(mal_cube_eq)
-        # Correct for pixel norm error
-        # fix overflow
-        bad = np.where(mal_cube > 2000)
-        # mal_cube[bad] = np.median(clean_cube)
-        for i in range(len(bad[0])):
-            neiborhood = cutCube(mal_cube, np.array([bad[0][i], bad[1][i], bad[2][i]]), (np.ones(3) * 5).astype(int),
-                                 -1000)
-            mal_cube[bad[0][i], bad[1][i], bad[2][i]] = np.mean(neiborhood)
-        # fix underflow
-        mal_cube[mal_cube < -1000] = -1000
+        mal_cube = deequalize(x_mal)
 
         ### Paste Location
         print("Pasting sample into scan")
@@ -196,18 +177,12 @@ class scan_manipulator:
         local_sample = clean_cube_unscaled
 
         # Init Touch-ups
-        if action == 'inject':  # inject type
-            noisemap = np.random.randn(150, 200, 300) * np.std(local_sample[local_sample < -600]) * .6
-            kernel_size = 3
-            factors = sigmoid((mal_cube_ext + 700) / 70)
-            k = kern01(mal_cube_ext.shape[0], kernel_size)
-            for i in range(factors.shape[0]):
-                factors[i, :, :] = factors[i, :, :] * k
-        else:  # remove type
-            noisemap = np.random.randn(150, 200, 200) * 30
-            kernel_size = .1
-            k = kern01(mal_cube_ext.shape[0], kernel_size)
-            factors = None
+        noisemap = np.random.randn(150, 200, 300) * np.std(local_sample[local_sample < -600]) * .6
+        kernel_size = 3
+        factors = sigmoid((mal_cube_ext + 700) / 70)
+        k = kern01(mal_cube_ext.shape[0], kernel_size)
+        for i in range(factors.shape[0]):
+            factors[i, :, :] = factors[i, :, :] * k
 
         # Perform touch-ups
         if config[
@@ -225,11 +200,7 @@ class scan_manipulator:
             noise = noisemap_s[:noise_map_dim, :noise_map_dim, :noise_map_dim]
         mal_cube_ext += noise
 
-        if action == 'inject':  # Injection
-            final_cube_s = np.maximum((mal_cube_ext * factors + ben_cube_ext * (1 - factors)), ben_cube_ext)
-        else:  # Removal
-            minv = np.min((np.min(mal_cube_ext), np.min(ben_cube_ext)))
-            final_cube_s = (mal_cube_ext + minv) * k + (ben_cube_ext + minv) * (1 - k) - minv
+        final_cube_s = np.maximum((mal_cube_ext * factors + ben_cube_ext * (1 - factors)), ben_cube_ext)
 
         self.scan = pasteCube(self.scan, final_cube_s, coord)
         print('touch-ups complete')
